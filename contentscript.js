a157701cffeaec2c6dc6dad49c69e0d9211d6165
Ethan Sherbondy <sherbondye@gmail.com>
2011-05-28 16:28:13 -0400

    Consolidating plugin with chrome version.

--- contentscript.js
+++ contentscript.js
@@ -3,5 +3,5 @@ var the_script = content.getElementsByTagName('script')[0];
 var re = new RegExp('(http://.+.js)');
 var the_src = re.exec(the_script.innerHTML);
 if (the_src) {
-    safari.self.tab.dispatchMessage("setScriptSrc", the_src[1]);
+    safari.self.tab.dispatchMessage('setScriptSrc', the_src[1]);
 }
